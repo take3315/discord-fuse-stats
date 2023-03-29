82b8f98a0c9fda43db806ff79a241f631d776538
take3315 <take3315.dfgc@gmail.com>
2023-03-29 22:30:43 +0700

    Update index.js
    fix balance to include interest accrual

--- index.js
+++ index.js
@@ -22,28 +22,28 @@ async function getdata() {
     
     const symbolETH = await fETHcontract.methods.symbol().call();
     const decimalsETH = await fETHcontract.methods.decimals().call();
-    const exchangeRateStoredETH = await fETHcontract.methods.exchangeRateStored().call();
-    const totalBorrowsETH = await fETHcontract.methods.totalBorrows().call();
+    const exchangeRateCurrentETH = await fETHcontract.methods.exchangeRateCurrent().call();
+    const totalBorrowsCurrentETH = await fETHcontract.methods.totalBorrowsCurrent().call();
     const totalSupplyETH = await fETHcontract.methods.totalSupply().call();
     const borrowcapETH = await comptroller.methods.borrowCaps(ADDRESS[0]).call();
     const borrowcapUSDC = await comptroller.methods.borrowCaps(ADDRESS[1]).call();
 
     const symbolUSDC = await fUSDCcontract.methods.symbol().call();
     const decimalsUSDC = await fUSDCcontract.methods.decimals().call();
-    const exchangeRateStoredUSDC = await fUSDCcontract.methods.exchangeRateStored().call();
-    const totalBorrowsUSDC = await fUSDCcontract.methods.totalBorrows().call();
+    const exchangeRateCurrentUSDC = await fUSDCcontract.methods.exchangeRateCurrent().call();
+    const totalBorrowsCurrentUSDC = await fUSDCcontract.methods.totalBorrowsCurrent().call();
     const totalSupplyUSDC = await fUSDCcontract.methods.totalSupply().call();
 
-    const oneCTETH = exchangeRateStoredETH / 10 ** 18;
+    const oneCTETH = exchangeRateCurrentETH / 10 ** 18;
     const supplyETH = totalSupplyETH / 10 ** decimalsETH * oneCTETH;
-    const borrowETH = totalBorrowsETH / 10 ** decimalsETH;
+    const borrowETH = totalBorrowsCurrentETH / 10 ** decimalsETH;
     const utilETH = borrowETH / supplyETH * 100
     const oldcapETH = borrowcapETH / 10 ** decimalsETH;
     const capETH = supplyETH * 0.8;
 
-    const oneCTUSDC = exchangeRateStoredUSDC / 10 ** 18;
+    const oneCTUSDC = exchangeRateCurrentUSDC / 10 ** 18;
     const supplyUSDC = totalSupplyUSDC / 10 ** decimalsUSDC * oneCTUSDC;
-    const borrowUSDC = totalBorrowsUSDC / 10 ** decimalsUSDC;
+    const borrowUSDC = totalBorrowsCurrentUSDC / 10 ** decimalsUSDC;
     const utilUSDC = borrowUSDC / supplyUSDC * 100;
     const oldcapUSDC = borrowcapUSDC / 10 ** decimalsUSDC;
     const capUSDC = supplyUSDC * 0.8;
